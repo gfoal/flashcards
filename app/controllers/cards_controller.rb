95aa39e312856f9c3b0c403697e9e9399686f8b0
Yan M <greenfoal@gmail.com>
2015-10-15 19:51:16 +0300

    added form creation for card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,31 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def create
+    card_params = params.require(:card).permit(:original_text, :translated_text)
+    card_params[:review_date] = Date.today + 3
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to(:cards => "index")
+    else
+      render :action => "new"
+    end
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
