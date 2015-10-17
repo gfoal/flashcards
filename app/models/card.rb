e5ba8082d39b3b160b9c8f3b56852454a4c16a89
Yan M <greenfoal@gmail.com>
2015-10-17 21:37:20 +0300

    Revert "Third task"

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,2 @@
 class Card < ActiveRecord::Base
-  validates :original_text, :translated_text, :review_date, presence: true
-  validate :inequality_of_original_and_translated_texts
-
-  def inequality_of_original_and_translated_texts
-    if original_text.downcase.eql? translated_text.downcase
-      errors.add(:translated_text, "Translated text cannot be same as original")
-    end
-  end
 end
